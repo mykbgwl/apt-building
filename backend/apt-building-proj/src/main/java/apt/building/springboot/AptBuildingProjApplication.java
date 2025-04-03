4e1e640a77f6fcfcdc05d8006ab3452049927281
Mayank <rockmb972@gmail.com>
2025-04-03 16:00:12 +0530

    Adding backend

---
+++ backend/apt-building-proj/src/main/java/apt/building/springboot/AptBuildingProjApplication.java
@@ -0,0 +1,13 @@
+package apt.building.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptBuildingProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptBuildingProjApplication.class, args);
+	}
+
+}
---
+++ backend/apt-building-proj/src/test/java/apt/building/springboot/AptBuildingProjApplicationTests.java
@@ -0,0 +1,13 @@
+package apt.building.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptBuildingProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
